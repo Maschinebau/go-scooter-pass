8cd8bdcca5b460d6a6e70820a3c51f7fdc48caa1
Osipov Alexander <saxmail0722@gmail.com>
2023-08-30 14:08:30 +0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <div className={styles.content}>
+      <h1>Hello, yandex team 😺</h1>
+    </div>
+  )
+}
+
+export default App
